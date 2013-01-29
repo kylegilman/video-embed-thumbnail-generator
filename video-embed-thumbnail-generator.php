425191041a2aaa976b4919c1400c175337db69c3
kylegilman <kylegilman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-29 16:01:04 +0000

    Updated version number in main php file
    git-svn-id: http://plugins.svn.wordpress.org/video-embed-thumbnail-generator/trunk@660709 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- video-embed-thumbnail-generator.php
+++ video-embed-thumbnail-generator.php
@@ -3,7 +3,7 @@
 Plugin Name: Video Embed & Thumbnail Generator
 Plugin URI: http://www.kylegilman.net/2011/01/18/video-embed-thumbnail-generator-wordpress-plugin/
 Description: Generates thumbnails, HTML5-compliant videos, and embed codes for locally hosted videos. Requires FFMPEG for thumbnails and encodes. <a href="options-general.php?page=video-embed-thumbnail-generator/video-embed-thumbnail-generator.php">Settings</a> | <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=kylegilman@gmail.com&item_name=Video%20Embed%20And%20Thumbnail%20Generator%20Plugin%20Donation/">Donate</a>
-Version: 3.0.2	
+Version: 3.0.3	
 Author: Kyle Gilman
 Author URI: http://www.kylegilman.net/
 
