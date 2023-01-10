9b63fa76747949399768619a387ae09b01f71c42
Kyle Gilman <kylegilman@gmail.com>
2023-01-10 16:24:39 -0500

    Moved embeddable-video.php

--- src/templates/embeddable-video.php
+++ src/templates/embeddable-video.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template for displaying a single embeddable video
+ * Template for displaying a single embeddable video and nothing else
  */
 global $content_width;
 $content_width_save = $content_width;
@@ -20,12 +20,14 @@ $kgvid_video_embed['enable'] = 'true';
 $shortcode = kgvid_generate_attachment_shortcode($kgvid_video_embed);
 
 ?>
-<html style="background-color:transparent;"><head>
+<html style="background-color:transparent;">
+<head>
 <?php wp_head(); ?>
 <style>body:before, body:after{ content: none; } .kgvid_wrapper { margin:0 !important; } <?php if ( array_key_exists('gallery', $kgvid_video_embed) ) { ?> .kgvid_below_video { color:white; } .kgvid_below_video a { color:aaa; }' <?php } ?></style>
-</head><body class="content" style="margin:0px; font-family: sans-serif; padding:0px; border:none; <?php if ( array_key_exists('gallery', $kgvid_video_embed) ) { ?> $html .= 'background-color:black; <?php } else { ?> background-color:transparent; <?php } ?> ">
+</head>
+<body class="content" style="margin:0px; font-family: sans-serif; padding:0px; border:none; <?php if ( array_key_exists('gallery', $kgvid_video_embed) ) { ?> $html .= 'background-color:black; <?php } else { ?> background-color:transparent; <?php } ?> ">
 <?php echo do_shortcode( $shortcode );
-wp_footer();
-?>
-</body></html>
+wp_footer(); ?>
+</body>
+</html>
 <?php $content_width = $content_width_save; //reset $content_width
