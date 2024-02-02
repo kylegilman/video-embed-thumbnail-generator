7ec478585d56affce380a6f1c427430eb7e5fd07
Kyle Gilman <kylegilman@gmail.com>
2024-02-02 10:31:02 -0500

    Attempting to fix change to uppercase directory names

---
+++ src/Admin/codec/Video_Codec_AV1.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Videopack\admin\codec;
+
+class Video_Codec_AV1 extends Video_Codec {
+	public function __construct() {
+		$properties = array(
+			'name'           => esc_html__( 'AV1 WEBM', 'video-embed-thumbnail-generator' ),
+			'label'          => esc_html__( 'AV1', 'video-embed-thumbnail-generator' ),
+			'suffix'         => 'av1',
+			'container'      => 'webm',
+			'mime'           => 'video/webm',
+			'codecs_string'  => 'av01',
+			'vcodec'         => 'libaom-av1',
+			'rate_control'   => array(
+				'crf' => array(
+					'min'     => 0,
+					'max'     => 63,
+					'default' => 31,
+				),
+				'vbr' => array(
+					'default'  => 1.23,
+					'constant' => -488.62,
+				),
+			),
+			'default_encode' => false,
+		);
+
+		parent::__construct( $properties );
+	}
+}
---
+++ src/Admin/codec/Video_Codec_VP9.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Videopack\Admin\Codec;
+
+class Video_Codec_VP9 extends Video_Codec {
+	public function __construct() {
+		$properties = array(
+			'name'           => esc_html__( 'VP9 WEBM', 'video-embed-thumbnail-generator' ),
+			'label'          => esc_html__( 'VP9', 'video-embed-thumbnail-generator' ),
+			'suffix'         => 'vp9',
+			'container'      => 'webm',
+			'mime'           => 'video/webm',
+			'codecs_string'  => 'vp09',
+			'vcodec'         => 'libvpx-vp9',
+			'rate_control'   => array(
+				'crf' => array(
+					'min'     => 0,
+					'max'     => 63,
+					'default' => 31,
+				),
+				'vbr' => array(
+					'default'  => 1.47,
+					'constant' => -121.63,
+				),
+			),
+			'default_encode' => false,
+		);
+
+		parent::__construct( $properties );
+	}
+}
