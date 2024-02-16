635031ca3d73f0b9ae7f2b250f905c7010b0e2d2
Kyle Gilman <kylegilman@gmail.com>
2024-02-16 10:47:39 -0500

    Continuing progress on Video_Input classes

--- src/Admin/Input/Video_Input.php
+++ src/Admin/Input/Video_Input.php
@@ -21,12 +21,6 @@ abstract class Video_Input {
 	protected $source;
 
 	/**
-	 * Array of video sources. Includes URLs and other metadata.
-	 * @var array $sources
-	 */
-	protected $sources;
-
-	/**
 	 * Videopack Options manager class instance
 	 * @var \Videopack\Admin\Options $options_manager
 	 */
@@ -38,6 +32,19 @@ abstract class Video_Input {
 	protected $options = array();
 
 	/**
+	 * Array of \Videopack\Admin\Formats\Video_Format objects.
+	 * @var array $video_formats
+	 */
+	protected $video_formats = array();
+
+	/**
+	 * Videopack video format ID.
+	 * @var string $format
+	 */
+	protected $format;
+
+	/**
+	 * Video type. ex. 'attachment_local', 'attachment_remote', 'url', etc.
 	 * @var string $type
 	 */
 	protected $type;
@@ -55,16 +62,39 @@ abstract class Video_Input {
 	protected $direct_path;
 
 	/**
-	 * Basename of the video file. Used for generating thumbnails, encoding video formats, and other file operations.
-	 * @var string $basename
+	 * Is the video source on the same server as the WordPress installation?
+	 * @var boolean $same_server
 	 */
-	protected $basename;
+	protected $same_server;
+
+	/**
+	 * Array of parts of the video path. Used for generating thumbnails, encoding video formats, and other file operations.
+	 * @var array $path_parts {
+	 *     @type string $dirname
+	 *     @type string $basename
+	 *     @type string $extension
+	 *     @type string $filename
+	 * }
+	 */
+	protected $path_parts;
 
 	/**
 	 * Descriptive title of the video.
 	 * @var string $name
 	 */
-	protected $post_title;
+	protected $title;
+
+	/**
+	 * MIME type of the video.
+	 * @var string $mime_type
+	 */
+	protected $mime_type;
+
+	/**
+	 * Video Codec.
+	 * @var \Videopack\Admin\Formats\Codecs\Video_Codec $codec
+	 */
+	protected $codec;
 
 	/**
 	 * @var int $width
@@ -91,11 +121,48 @@ abstract class Video_Input {
 	 */
 	protected $metadata = array();
 
-	public function __construct( $source, string $type, \Videopack\Admin\Options $options_manager ) {
+	/**
+	 * Does the video source exist?
+	 * @var boolean $exists
+	 */
+	protected $exists;
+
+	/**
+	 * Is the video source compatible with Videopack?
+	 * @var bool $compatible
+	 */
+	protected $compatible;
+
+	/**
+	 * Array of \Videopack\Admin\Formats\Video_Input objects.
+	 * @var array $child_sources
+	 */
+	protected $child_sources;
+
+	/**
+	 * Video_Input constructor.
+	 *
+	 * @param string|int $source
+	 * @param string $type
+	 * @param \Videopack\Admin\Options $options_manager
+	 * @param string|null $format Videopack video format ID.
+	 * @param bool|null $exists
+	 */
+
+	public function __construct(
+		$source,
+		string $type,
+		\Videopack\Admin\Options $options_manager,
+		$format = null,
+		$exists = null
+	) {
 		$this->source          = $source;
 		$this->type            = $type;
 		$this->options_manager = $options_manager;
 		$this->options         = $options_manager->get_options();
+		$this->video_formats   = $this->options_manager->get_video_formats();
+		$this->format          = $format;
+		$this->exists          = $exists;
 	}
 
 	abstract protected function fetch_metadata();
@@ -128,6 +195,15 @@ abstract class Video_Input {
 		return $this->url;
 	}
 
+	abstract protected function set_exists(): void;
+
+	public function exists(): bool {
+		if ( $this->exists === null ) {
+			$this->set_exists();
+		}
+		return $this->exists;
+	}
+
 	abstract protected function set_direct_path(): void;
 
 	public function get_direct_path(): string {
@@ -137,22 +213,162 @@ abstract class Video_Input {
 		return $this->direct_path;
 	}
 
-	abstract protected function set_basename(): void;
+	abstract protected function set_same_server(): void;
 
-	public function get_basename(): string {
-		if ( ! $this->basename ) {
-			$this->set_basename();
+	public function is_same_server(): bool {
+		if ( ! isset( $this->same_server ) ) {
+			$this->set_same_server();
 		}
-		return $this->basename;
+		return $this->same_server;
 	}
 
-	abstract protected function set_post_title(): void;
+	protected function set_path_parts(): void {
 
-	public function get_post_title(): string {
-		if ( ! $this->post_title ) {
-			$this->set_post_title();
+		$pathinfo   = pathinfo( $this->get_direct_path() );
+		$path_parts = array(
+			'dirname'   => $pathinfo['dirname'],
+			'basename'  => sanitize_file_name( $pathinfo['basename'] ),
+			'extension' => $pathinfo['extension'],
+			'filename'  => sanitize_file_name( $pathinfo['filename'] ),
+		);
+
+		$path_parts['no_extension'] = $pathinfo['dirname'] . '/' . $pathinfo['filename'];
+
+		$this->path_parts = $path_parts;
+	}
+
+	public function get_dirname(): string {
+		if ( ! $this->path_parts ) {
+			$this->set_path_parts();
 		}
-		return $this->post_title;
+		return $this->path_parts['dirname'];
+	}
+
+	public function get_basename(): string {
+		if ( ! $this->path_parts ) {
+			$this->set_path_parts();
+		}
+		return $this->path_parts['basename'];
+	}
+
+	public function get_extension(): string {
+		if ( ! $this->path_parts ) {
+			$this->set_path_parts();
+		}
+		return $this->path_parts['extension'];
+	}
+
+	public function get_filename(): string {
+		if ( ! $this->path_parts ) {
+			$this->set_path_parts();
+		}
+		return $this->path_parts['filename'];
+	}
+
+	public function get_no_extension(): string {
+		if ( ! $this->path_parts ) {
+			$this->set_path_parts();
+		}
+		return $this->path_parts['no_extension'];
+	}
+
+	protected function set_compatible(): void {
+
+		$compatible = array(
+			'mp4',
+			'mov',
+			'm4v',
+			'ogv',
+			'ogg',
+			'webm',
+			'mkv',
+			'mpd',
+			'm3u8',
+		);
+
+		/**
+		 * Filter the list of Videopack-compatible video extensions.
+		 * @param array $compatible Array of compatible video extensions.
+		 */
+		$compatible = apply_filters( 'videopack_compatible_extensions', $compatible );
+
+		$this->compatible = in_array( $this->get_extension(), $compatible, true );
+	}
+
+	public function is_compatible(): bool {
+		if ( ! isset( $this->compatible ) ) {
+			$this->set_compatible();
+		}
+		return $this->compatible;
+	}
+
+	abstract protected function set_title(): void;
+
+	public function get_title(): string {
+		if ( ! $this->title ) {
+			$this->set_title();
+		}
+		return $this->title;
+	}
+
+	abstract protected function set_mime_type(): void;
+
+	public function get_mime_type(): string {
+		if ( ! $this->mime_type ) {
+			$this->set_mime_type();
+		}
+		return $this->mime_type;
+	}
+
+	public function get_preferred_codecs(): array {
+		$preferred_codecs = array(
+			'video/mp4'  => 'h264',
+			'video/webm' => 'vp9',
+		);
+		/**
+		 * Filter preferred video codecs for a mime type. Since multiple codecs can be available for a mime type,
+		 * this filter can change or provide a preferred codec for a mime type.
+		 * @param array $preferred_codecs Array of preferred video codecs.
+		 * @return array
+		 * @since 5.0.0
+		 */
+		return apply_filters( 'videopack_preferred_codecs', $preferred_codecs );
+	}
+
+	protected function get_codec_by_mime_type() {
+
+		if ( $this->get_mime_type() ) {
+			$codecs         = $this->options_manager->get_video_codecs();
+			$same_mime_type = array();
+			foreach ( $codecs as $codec ) {
+				if ( $codec->get_mime_type() === $this->get_mime_type() ) {
+					$same_mime_type[] = $codec;
+				}
+			}
+
+			if ( count( $same_mime_type ) === 1 ) {
+				//only one available codec with the same mime type
+				return $same_mime_type[0];
+			} elseif ( count( $same_mime_type ) > 1 ) {
+				//multiple available codecs with the same mime type
+				$preferred_codecs = $this->get_preferred_codecs();
+				if ( array_key_exists( $this->get_mime_type(), $preferred_codecs )
+					&& $preferred_codecs[ $this->get_mime_type() ] === $codec->get_id()
+				) {
+					return $codec;
+				}
+			}
+		}
+		return false;
+	}
+
+	abstract protected function set_codec(): void;
+
+	public function get_codec(): \Videopack\Admin\Formats\Codecs\Video_Codec {
+		if ( ! $this->codec ) {
+			$this->set_codec();
+		}
+		return $this->codec;
 	}
 
 	abstract protected function set_dimensions();
@@ -197,6 +413,92 @@ abstract class Video_Input {
 		}
 
 		return $this->duration;
+	}
+
+	public function is_original(): bool {
+		return $this->format === 'original';
+	}
+
+	public function get_video_formats(): array {
+		return $this->video_formats;
+	}
+
+	public function get_child_sources(): array {
+
+		if ( $this->is_original() && empty( $this->child_sources ) ) {
+			$this->set_child_sources();
+		}
+		return $this->child_sources;
+	}
+
+	public function get_existing_child_sources(): array {
+
+		$existing_child_sources = array();
+
+		if ( $this->is_original() && empty( $this->child_sources ) ) {
+			$this->set_child_sources();
+		}
+
+		if ( $this->get_child_sources() ) {
+			foreach ( $this->get_child_sources() as $child_source ) {
+				if ( $child_source->exists() ) {
+					$existing_child_sources[] = $child_source;
+				}
+			}
+		}
+
+		return $existing_child_sources;
+	}
+
+	abstract protected function set_child_sources(): void;
+
+	protected function find_attachment_children(): array {
+		if ( is_numeric( $this->source ) ) {
+			$args = array(
+				'numberposts' => -1,
+				'post_parent' => $this->source,
+				'post_type'   => 'attachment',
+			);
+
+			return get_posts( $args );
+		}
+
+		return array();
+	}
+
+	protected function find_attachment_by_externalurl_meta_key(): array {
+		$args = array(
+			'numberposts' => -1,
+			'post_type'   => 'attachment',
+			'meta_key'    => '_kgflashmediaplayer-externalurl',
+			'meta_value'  => esc_url_raw( rawurldecode( $this->url ) ),
+		);
+
+		return get_posts( $args );
+	}
+
+
+	protected function is_format_in_same_directory( string $suffix ) {
+		$file = $this->get_no_extension() . $suffix;
+		if ( file_exists( $file ) ) {
+			return $file;
+		}
+		return false;
+	}
+
+	public function get_video_player_source(): array {
+
+		$video_player_source = array(
+			'src'   => $this->get_url(),
+			'type'  => $this->get_mime_type(),
+			'codec' => '',
+		);
+
+		if ( $this->get_codec() ) {
+			$video_player_source['codec'] = $this->get_codec()->get_fourcc();
+		}
+
+		return $video_player_source;
 	}
 
 	protected function handle_error( string $error ): void {
--- src/Admin/Input/Video_Input_Attachment_Local.php
+++ src/Admin/Input/Video_Input_Attachment_Local.php
@@ -6,10 +6,15 @@ class Video_Input_Attachment_Local extends Video_Input {
 
 	protected $meta_manager = null;
 
-	public function __construct( int $attachment_id, \Videopack\Admin\Options $options_manager ) {
+	public function __construct(
+		int $attachment_id,
+		\Videopack\Admin\Options $options_manager,
+		$format = null,
+		$exists = false
+	) {
 
 		if ( $this->validate_source( $attachment_id ) ) {
-			parent::__construct( $attachment_id, 'attachment_local', $options_manager );
+			parent::__construct( $attachment_id, 'attachment_local', $options_manager, $format, $exists );
 			$this->fetch_metadata();
 		} else {
 			throw new \Exception( 'Invalid attachment ID.' );
@@ -32,16 +37,43 @@ class Video_Input_Attachment_Local extends Video_Input {
 		$this->url = wp_get_attachment_url( $this->source );
 	}
 
+	protected function set_exists(): void {
+		$this->exists = file_exists( get_attached_file( $this->source ) );
+	}
+
 	protected function set_direct_path(): void {
 		$this->direct_path = get_attached_file( $this->source );
 	}
 
-	protected function set_basename(): void {
-		$this->basename = sanitize_file_name( basename( $this->direct_path ) );
+	protected function set_same_server(): void {
+		$this->same_server = true;
 	}
 
-	protected function set_post_title(): void {
-		$this->post_title = get_the_title( $this->source );
+	protected function set_title(): void {
+		$this->title = get_the_title( $this->source );
+	}
+
+	protected function set_mime_type(): void {
+		$this->mime_type = get_post_mime_type( $this->source );
+	}
+
+	protected function set_codec(): void {
+
+		$codecs = $this->options_manager->get_video_codecs();
+
+		if ( isset( $this->metadata['fourcc'] ) ) {
+			foreach ( $codecs as $codec ) {
+				if ( $codec->get_fourcc() === $this->metadata['fourcc'] ) {
+					$this->codec = $codec;
+					break;
+				}
+			}
+		} else {
+			$codec = $this->get_codec_by_mime_type();
+			if ( $codec ) {
+				$this->codec = $codec;
+			}
+		}
 	}
 
 	public function fetch_metadata(): void {
@@ -64,5 +96,36 @@ class Video_Input_Attachment_Local extends Video_Input {
 
 	protected function set_duration(): void {
 		$this->duration = $this->metadata['duration'];
+	}
+
+	protected function set_child_sources(): void {
+
+		$children = $this->find_attachment_children();
+
+		foreach ( $this->video_formats as $format => $format_object ) {
+			if ( $children ) {
+				foreach ( $children as $child ) {
+					$meta_format = get_post_meta( $child->ID, '_kgflashmediaplayer-format', true );
+					if ( $meta_format === $format ) {
+						$this->child_sources[ $format ] = new Video_Input_Attachment_Local(
+							$child->ID,
+							$this->options_manager,
+							$format,
+							true
+						);
+						break;
+					}
+				}
+			}
+			$same_directory = $this->is_format_in_same_directory( $format_object->get_suffix() );
+			if ( $same_directory ) {
+				$this->child_sources[ $format ] = new Video_Input_Attachment_Local(
+					$this->source,
+					$this->options_manager,
+					$format,
+					true
+				);
+			}
+		}
 	}
 }
