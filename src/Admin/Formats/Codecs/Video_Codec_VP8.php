a73f6f8918af30b996ad172077a72f44db8e0c38
Kyle Gilman <kylegilman@gmail.com>
2024-02-20 10:16:41 -0500

    Added OGV and VP8 codecs for backward compatibility

---
+++ src/Admin/Formats/Codecs/Video_Codec_VP8.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Videopack\Admin\Formats\Codecs;
+
+class Video_Codec_VP8 extends Video_Codec {
+	public function __construct() {
+		$properties = array(
+			'name'           => esc_html__( 'VP8 WEBM', 'video-embed-thumbnail-generator' ),
+			'label'          => esc_html__( 'VP8', 'video-embed-thumbnail-generator' ),
+			'id'             => 'vp8',
+			'container'      => 'webm',
+			'mime'           => 'video/webm',
+			'fourcc'         => 'vp80',
+			'vcodec'         => 'libvpx',
+			'rate_control'   => array(
+				'crf' => array(
+					'min'     => 4,
+					'max'     => 63,
+					'default' => 10,
+				),
+				'vbr' => array(
+					'default'  => 1.89,
+					'constant' => 469,
+				),
+			),
+			'default_encode' => false,
+		);
+
+		parent::__construct( $properties );
+	}
+}
