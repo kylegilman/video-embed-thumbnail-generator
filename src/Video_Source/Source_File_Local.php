2bbf5454538c616723281e7235d9337e6fa9a361
Kyle Gilman <kylegilman@gmail.com>
2024-02-20 10:14:31 -0500

    Changed EOL characters

---
+++ src/Video_Source/Source_File_Local.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Videopack\Video_Source;
+
+class Source_File_Local extends Source {
+
+	public function __construct(
+		string $source,
+		\Videopack\Admin\Options $options_manager,
+		$format = null,
+		$exists = null,
+		$parent_id = null
+	) {
+
+		if ( $this->validate_source( $source ) ) {
+			parent::__construct( $source, 'file_local', $options_manager, $format, $exists, $parent_id );
+		} else {
+			throw new \Exception( esc_html__( 'Source is empty.', 'video-embed-thumbnail-generator' ) );
+		}
+	}
+
+	protected function set_url(): void {
+		$this->url = str_replace( ABSPATH, site_url( '/' ), $this->source );
+	}
+
+	protected function set_exists(): void {
+		$this->exists = file_exists( $this->source );
+	}
+
+	protected function set_direct_path(): void {
+		$this->direct_path = $this->source;
+	}
+
+	protected function set_local(): void {
+		$this->local = true;
+	}
+
+	protected function set_parent_id(): void {
+		$this->get_current_post_id();
+	}
+
+	protected function set_title(): void {
+		$this->title = basename( $this->source );
+	}
+
+	protected function set_mime_type(): void {
+		$this->mime_type = mime_content_type( $this->source );
+	}
+
+	public function set_metadata(): void {
+		$this->metadata = wp_read_video_metadata( $this->source );
+	}
+
+	protected function set_child_sources(): void {
+
+		foreach ( $this->video_formats as $format ) {
+
+			$same_directory = $this->find_format_in_same_directory( $format );
+			if ( $same_directory ) {
+				continue;
+			}
+
+			$this->create_source_placeholder( $format );
+		}
+	}
+}
