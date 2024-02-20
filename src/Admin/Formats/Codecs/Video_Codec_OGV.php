629a8baeef397870c9e5d252c5439b43e868e9be
Kyle Gilman <kylegilman@gmail.com>
2024-02-20 10:16:41 -0500

    Added OGV and VP8 codecs for backward compatibility

---
+++ src/Admin/Formats/Codecs/Video_Codec_OGV.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Videopack\Admin\Formats\Codecs;
+
+class Video_Codec_OGV extends Video_Codec {
+	public function __construct() {
+		$properties = array(
+			'name'           => esc_html__( 'OGV', 'video-embed-thumbnail-generator' ),
+			'label'          => esc_html__( 'OGV', 'video-embed-thumbnail-generator' ),
+			'id'             => 'ogv',
+			'container'      => 'ogv',
+			'mime'           => 'video/ogg',
+			'fourcc'         => 'theo',
+			'vcodec'         => 'libtheora',
+			'rate_control'   => array(
+				'crf' => array(
+					'min'     => 0,
+					'max'     => 10,
+					'default' => 6,
+				),
+				'vbr' => array(
+					'default'  => false,
+					'constant' => false,
+				),
+			),
+			'default_encode' => false,
+		);
+
+		parent::__construct( $properties );
+	}
+}
