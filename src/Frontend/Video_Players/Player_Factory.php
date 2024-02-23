16d17f54a044541643c723e62147d726d5a74f82
Kyle Gilman <kylegilman@gmail.com>
2024-02-23 16:06:33 -0500

    Building out Players, removed need for separate frontend Assets class

---
+++ src/Frontend/Video_Players/Player_Factory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Videopack\Frontend\Video_Players;
+
+class Player_Factory {
+
+	/**
+	 * Create a video player.
+	 *
+	 * @param \Videopack\Admin\Options $options_manager The options manager.
+	 *
+	 * @return Player
+	 */
+	public static function create( \Videopack\Admin\Options $options_manager ): Player {
+		$player  = null;
+		$options = $options_manager->get_options();
+
+		switch ( $options['embed_method'] ) {
+			case 'Video.js v8':
+				$player = new Player_Video_Js( $options_manager );
+				break;
+			case 'WordPress Default':
+				$player = new Player_WordPress_Default( $options_manager );
+				break;
+		}
+
+		return apply_filters( 'videopack_video_player', $player, $options_manager );
+	}
+}
--- src/Frontend/Video_Players/Player_WordPress_Default.php
+++ src/Frontend/Video_Players/Player_WordPress_Default.php
@@ -4,4 +4,48 @@ namespace Videopack\Frontend\Video_Players;
 
 class Player_WordPress_Default extends Player {
 
+	public function register_scripts() {
+
+		parent::register_scripts();
+
+		wp_register_script(
+			'mejs_sourcechooser',
+			plugins_url( '/js/mejs-source-chooser.js', __FILE__ ),
+			array( 'mediaelement' ),
+			VIDEOPACK_VERSION,
+			true
+		);
+
+		wp_register_script(
+			'mejs-speed',
+			plugins_url( '/js/mejs-speed.js', __FILE__ ),
+			array( 'mediaelement' ),
+			VIDEOPACK_VERSION,
+			true
+		);
+	}
+
+	public function enqueue_styles() {
+
+		parent::enqueue_styles();
+
+		wp_enqueue_style( 'video-js', plugins_url( '', dirname( __DIR__ ) ) . '/video-js/video-js.min.css', '', $this->options['videojs_version'] ); // gives access to video-js icons for resolution gear selector and social logos
+	}
+
+	protected function video(): string {
+		$executed_shortcode = wp_video_shortcode( $this->wp_shortcode_atts() );
+		return $executed_shortcode;
+	}
+
+	protected function wp_shortcode_atts(): array {
+		$shortcode_atts = array(
+			'src'      => strtok( $this->get_main_source_url(), '?' ), // remove query string
+			'poster'   => $this->get_poster(),
+			'loop'     => $this->atts['loop'],
+			'autoplay' => $this->atts['autoplay'],
+			'muted'    => $this->atts['muted'],
+			'preload'  => $this->atts['preload'],
+		);
+		return $shortcode_atts;
+	}
 }
