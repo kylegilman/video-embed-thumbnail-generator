becb96e9a10825f6e78df294f37d29c8cdd24890
Kyle Gilman <kylegilman@gmail.com>
2024-03-22 09:00:28 -0400

    Updated symfony/process

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'kylegilman/video-embed-thumbnail-generator',
-        'pretty_version' => '4.9.x-dev',
-        'version' => '4.9.9999999.9999999-dev',
-        'reference' => '4e4847e1eb7b6608d4e9c5c02e417220c93cacce',
+        'pretty_version' => '4.10.x-dev',
+        'version' => '4.10.9999999.9999999-dev',
+        'reference' => '8a4c94c57c8c6eb7d26c814c2b02c7b10076d06e',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -20,9 +20,9 @@
             'dev_requirement' => false,
         ),
         'kylegilman/video-embed-thumbnail-generator' => array(
-            'pretty_version' => '4.9.x-dev',
-            'version' => '4.9.9999999.9999999-dev',
-            'reference' => '4e4847e1eb7b6608d4e9c5c02e417220c93cacce',
+            'pretty_version' => '4.10.x-dev',
+            'version' => '4.10.9999999.9999999-dev',
+            'reference' => '8a4c94c57c8c6eb7d26c814c2b02c7b10076d06e',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -38,9 +38,9 @@
             'dev_requirement' => false,
         ),
         'symfony/process' => array(
-            'pretty_version' => 'v5.4.35',
-            'version' => '5.4.35.0',
-            'reference' => 'cbc28e34015ad50166fc2f9c8962d28d0fe861eb',
+            'pretty_version' => 'v5.4.36',
+            'version' => '5.4.36.0',
+            'reference' => '4fdf34004f149cc20b2f51d7d119aa500caad975',
             'type' => 'library',
             'install_path' => __DIR__ . '/../symfony/process',
             'aliases' => array(),
