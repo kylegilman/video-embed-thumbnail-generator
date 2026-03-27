ab950dde185574abeb5b57bdc22bcf7b7ea40758
Kyle Gilman <4528113+kylegilman@users.noreply.github.com>
2026-03-27 16:41:41 -0400

    Added play/pause svg icons

--- admin-ui/src/assets/icon.js
+++ admin-ui/src/assets/icon.js
@@ -98,6 +98,30 @@ const sortDescending = (
 	</svg>
 );
 
+const play = (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		height="24px"
+		viewBox="0 0 24 24"
+		width="24px"
+		fill="currentColor"
+	>
+		<path d="M8 5v14l11-7z" />
+	</svg>
+);
+
+const pause = (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		height="24px"
+		viewBox="0 0 24 24"
+		width="24px"
+		fill="currentColor"
+	>
+		<path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z" />
+	</svg>
+);
+
 export {
 	insertImage,
 	save,
@@ -106,4 +130,6 @@ export {
 	volumeUp,
 	sortAscending,
 	sortDescending,
+	play,
+	pause,
 };
