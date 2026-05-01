c1a6e13217ed4a1526413e2288c9d4192938d58c
Kyle Gilman <4528113+kylegilman@users.noreply.github.com>
2026-05-01 16:07:21 -0400

    Added custom icons for every block

--- admin-ui/src/assets/icon.js
+++ admin-ui/src/assets/icon.js
@@ -1,25 +1,759 @@
 const videopack = (
-	<svg
-		width="20px"
-		height="20px"
-		xmlns="http://www.w3.org/2000/svg"
-		viewBox="0 0 395.11 395.11"
-	>
-		<g data-name="Ellipse 1" transform="rotate(-45 -201.149 592.789)">
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400">
+		<g transform="rotate(-45 200.518 199.773)">
 			<circle
-				cx="360.24"
-				cy="595.24"
-				r="182.56"
-				fill="none"
-				stroke="#cd0000"
-				strokeWidth="30"
-			></circle>
+				cx={200.52}
+				cy={199.77}
+				r={182.56}
+				style={{
+					fill: '#fff',
+				}}
+			/>
+			<circle
+				cx={200.52}
+				cy={199.77}
+				r={182.56}
+				style={{
+					fill: 'none',
+					stroke: '#cd0000',
+					strokeMiterlimit: 100,
+					strokeWidth: 30,
+				}}
+			/>
 		</g>
 		<path
-			fill="#cd0000"
-			d="M95.4 108.3h45.81l57.42 98.69 55.57-98.69h47.49L198.54 286.81 95.4 109.68"
-		></path>
-		<path fill="#f26a6b" d="M254.2 108.3l-55.57 98.69-57.42-98.69"></path>
+			style={{
+				fill: '#cd0000',
+			}}
+			d="M98.37 124.52h45.81l57.42 98.69 55.57-98.69h47.48L201.51 303.03 98.37 125.9"
+		/>
+		<path
+			style={{
+				fill: '#ff9ca1',
+			}}
+			d="m257.17 124.52-55.57 98.69-57.42-98.69"
+		/>
+	</svg>
+);
+
+const videopackCaption = (
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400">
+		<path
+			d="M43.63 341.01c-12.65 0-23-9.2-23-22.61v-.77c0-13.03 9.97-22.61 23-22.61s23.38 9.2 23.38 22.61v.77c0 13.42-9.97 22.61-23.38 22.61m69.37 0c-12.65 0-23-9.2-23-22.61v-.77c0-13.03 9.97-22.61 23-22.61s23.38 9.2 23.38 22.61v.77c0 13.42-9.97 22.61-23.38 22.61m69.37 0c-12.65 0-23-9.2-23-22.61v-.77c0-13.03 9.97-22.61 23-22.61s23.38 9.2 23.38 22.61v.77c0 13.42-9.97 22.61-23.38 22.61"
+			style={{
+				fill: '#cd0000',
+			}}
+		/>
+		<circle
+			cx={198.31}
+			cy={159.72}
+			r={69.82}
+			style={{
+				fill: '#fff',
+			}}
+		/>
+		<circle
+			cx={198.31}
+			cy={159.72}
+			r={69.82}
+			style={{
+				fill: 'none',
+				stroke: '#cd0000',
+				strokeMiterlimit: 100,
+				strokeWidth: '11.47px',
+			}}
+		/>
+		<path
+			style={{
+				fill: '#cd0000',
+			}}
+			d="M169.53 198.78v-17.51l37.74-21.96-37.74-21.26v-18.16l68.27 39.45-67.74 39.44"
+		/>
+		<path
+			style={{
+				fill: '#ff9ca1',
+			}}
+			d="m169.53 138.05 37.74 21.26-37.74 21.96"
+		/>
+		<path
+			style={{
+				fill: 'none',
+				stroke: '#cd0000',
+				strokeMiterlimit: 100,
+				strokeWidth: 19,
+			}}
+			d="M14.4 55.65h372.22V264.9H14.4z"
+		/>
+	</svg>
+);
+
+const videopackCollection = (
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400">
+		<path
+			style={{
+				fill: 'none',
+				stroke: '#cd0000',
+				strokeMiterlimit: 100,
+				strokeWidth: 14,
+			}}
+			d="M12.01 84.61h170.53v93.84H12.01zm210.09 0h170.53v93.84H222.1zM12.01 221.62h170.53v93.84H12.01zm210.09 0h170.53v93.84H222.1z"
+		/>
+	</svg>
+);
+
+const videopackDuration = (
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400">
+		<path
+			d="m67.87 129.5-8.65 32.21h26.11l8.65-32.21h15.26l-8.65 32.21h17.29v12.71H97.2L90.59 199h18.99v12.71H87.2l-8.31 31.36H63.63l8.31-31.36H45.83l-8.31 31.36H22.26l8.31-31.36H12.43V199h21.53l6.61-24.58H20.74v-12.71h23.22l8.65-32.21zm-18.65 69.51h26.11l6.61-24.58H55.83zm94.08-5.09c-5.59 0-10.17-4.07-10.17-10v-.34c0-5.76 4.41-10 10.17-10s10.34 4.07 10.34 10v.34c0 5.93-4.41 10-10.34 10m0 50.86c-5.59 0-10.17-4.07-10.17-10v-.34c0-5.76 4.41-10 10.17-10s10.34 4.07 10.34 10v.34c0 5.93-4.41 10-10.34 10m81.03-115.28-8.65 32.21h26.11l8.65-32.21h15.26l-8.65 32.21h17.29v12.71h-20.68L247.05 199h18.99v12.71h-22.38l-8.31 31.36h-15.26l8.31-31.36h-26.11l-8.31 31.36h-15.26l8.31-31.36h-18.14V199h21.53l6.61-24.58H177.2v-12.71h23.22l8.65-32.21zm-18.65 69.51h26.11l6.61-24.58h-26.11zm135.96-69.51-8.65 32.21h26.11l8.65-32.21h15.26l-8.65 32.21h17.29v12.71h-20.68L364.36 199h18.99v12.71h-22.38l-8.31 31.36H337.4l8.31-31.36H319.6l-8.31 31.36h-15.26l8.31-31.36H286.2V199h21.53l6.61-24.58h-19.83v-12.71h23.22l8.65-32.21zm-18.65 69.51h26.11l6.61-24.58H329.6z"
+			style={{
+				fill: '#cd0000',
+			}}
+		/>
+	</svg>
+);
+
+const videopackGallery = (
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400">
+		<path
+			style={{
+				fill: 'none',
+				stroke: '#cd0000',
+				strokeMiterlimit: 100,
+				strokeWidth: 14,
+			}}
+			d="M8.14 84.54h170.53v93.84H8.14z"
+		/>
+		<g transform="rotate(-5.65 92.234 131.62)">
+			<circle
+				cx={92.16}
+				cy={131.51}
+				r={34.87}
+				style={{
+					fill: '#fff',
+				}}
+			/>
+			<circle
+				cx={92.16}
+				cy={131.51}
+				r={34.87}
+				style={{
+					fill: 'none',
+					stroke: '#cd0000',
+					strokeMiterlimit: 100,
+					strokeWidth: '5.73px',
+				}}
+			/>
+		</g>
+		<path
+			style={{
+				fill: '#cd0000',
+			}}
+			d="M77.79 151.02v-8.75l18.85-10.97-18.85-10.61v-9.07l34.1 19.7-33.84 19.7"
+		/>
+		<path
+			style={{
+				fill: '#ff9ca1',
+			}}
+			d="m77.79 120.69 18.85 10.61-18.85 10.97"
+		/>
+		<path
+			style={{
+				fill: 'none',
+				stroke: '#cd0000',
+				strokeMiterlimit: 100,
+				strokeWidth: 14,
+			}}
+			d="M220.87 84.54H391.4v93.84H220.87z"
+		/>
+		<g transform="rotate(-5.65 309.192 131.66)">
+			<circle
+				cx={308.89}
+				cy={131.51}
+				r={34.87}
+				style={{
+					fill: '#fff',
+				}}
+			/>
+			<circle
+				cx={308.89}
+				cy={131.51}
+				r={34.87}
+				style={{
+					fill: 'none',
+					stroke: '#cd0000',
+					strokeMiterlimit: 100,
+					strokeWidth: '5.73px',
+				}}
+			/>
+		</g>
+		<path
+			style={{
+				fill: '#cd0000',
+			}}
+			d="M294.51 151.02v-8.75l18.85-10.97-18.85-10.61v-9.07l34.1 19.7-33.83 19.7"
+		/>
+		<path
+			style={{
+				fill: '#ff9ca1',
+			}}
+			d="m294.51 120.69 18.85 10.61-18.85 10.97"
+		/>
+		<path
+			style={{
+				fill: 'none',
+				stroke: '#cd0000',
+				strokeMiterlimit: 100,
+				strokeWidth: 14,
+			}}
+			d="M8.14 221.62h170.53v93.84H8.14z"
+		/>
+		<g transform="rotate(-5.65 92.268 268.846)">
+			<circle
+				cx={92.16}
+				cy={268.58}
+				r={34.87}
+				style={{
+					fill: '#fff',
+				}}
+			/>
+			<circle
+				cx={92.16}
+				cy={268.58}
+				r={34.87}
+				style={{
+					fill: 'none',
+					stroke: '#cd0000',
+					strokeMiterlimit: 100,
+					strokeWidth: '5.73px',
+				}}
+			/>
+		</g>
+		<path
+			style={{
+				fill: '#cd0000',
+			}}
+			d="M77.79 288.09v-8.75l18.85-10.96-18.85-10.62v-9.07l34.1 19.7-33.84 19.7"
+		/>
+		<path
+			style={{
+				fill: '#ff9ca1',
+			}}
+			d="m77.79 257.76 18.85 10.62-18.85 10.96"
+		/>
+		<path
+			style={{
+				fill: 'none',
+				stroke: '#cd0000',
+				strokeMiterlimit: 100,
+				strokeWidth: 14,
+			}}
+			d="M220.87 221.62H391.4v93.84H220.87z"
+		/>
+		<g transform="rotate(-5.65 309.13 268.78)">
+			<circle
+				cx={308.89}
+				cy={268.58}
+				r={34.87}
+				style={{
+					fill: '#fff',
+				}}
+			/>
+			<circle
+				cx={308.89}
+				cy={268.58}
+				r={34.87}
+				style={{
+					fill: 'none',
+					stroke: '#cd0000',
+					strokeMiterlimit: 100,
+					strokeWidth: '5.73px',
+				}}
+			/>
+		</g>
+		<path
+			style={{
+				fill: '#cd0000',
+			}}
+			d="M294.51 288.09v-8.75l18.85-10.96-18.85-10.62v-9.07l34.1 19.7-33.83 19.7"
+		/>
+		<path
+			style={{
+				fill: '#ff9ca1',
+			}}
+			d="m294.51 257.76 18.85 10.62-18.85 10.96"
+		/>
+	</svg>
+);
+
+const videopackList = (
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400">
+		<path
+			style={{
+				fill: 'none',
+				stroke: '#cd0000',
+				strokeMiterlimit: 100,
+				strokeWidth: 14,
+			}}
+			d="M111.79 6.56h185.1v101.85h-185.1z"
+		/>
+		<g transform="rotate(-5.65 205.384 57.57)">
+			<circle
+				cx={205.16}
+				cy={57.53}
+				r={37.85}
+				style={{
+					fill: '#fff',
+				}}
+			/>
+			<circle
+				cx={205.16}
+				cy={57.53}
+				r={37.85}
+				style={{
+					fill: 'none',
+					stroke: '#cd0000',
+					strokeMiterlimit: 100,
+					strokeWidth: '6.22px',
+				}}
+			/>
+		</g>
+		<path
+			style={{
+				fill: '#cd0000',
+			}}
+			d="M189.56 78.71v-9.49l20.46-11.91-20.46-11.52v-9.84l37 21.38-36.72 21.38"
+		/>
+		<path
+			style={{
+				fill: '#ff9ca1',
+			}}
+			d="m189.56 45.79 20.46 11.52-20.46 11.91"
+		/>
+		<path
+			style={{
+				fill: 'none',
+				stroke: '#cd0000',
+				strokeMiterlimit: 100,
+				strokeWidth: 14,
+			}}
+			d="M111.79 148.88h185.1v101.85h-185.1z"
+		/>
+		<g transform="rotate(-5.65 205.365 199.974)">
+			<circle
+				cx={205.16}
+				cy={199.85}
+				r={37.85}
+				style={{
+					fill: '#fff',
+				}}
+			/>
+			<circle
+				cx={205.16}
+				cy={199.85}
+				r={37.85}
+				style={{
+					fill: 'none',
+					stroke: '#cd0000',
+					strokeMiterlimit: 100,
+					strokeWidth: '6.22px',
+				}}
+			/>
+		</g>
+		<path
+			style={{
+				fill: '#cd0000',
+			}}
+			d="M189.56 221.03v-9.5l20.46-11.9-20.46-11.52v-9.85l37 21.39-36.72 21.38"
+		/>
+		<path
+			style={{
+				fill: '#ff9ca1',
+			}}
+			d="m189.56 188.11 20.46 11.52-20.46 11.9"
+		/>
+		<path
+			style={{
+				fill: 'none',
+				stroke: '#cd0000',
+				strokeMiterlimit: 100,
+				strokeWidth: 14,
+			}}
+			d="M111.79 290.2h185.1v101.85h-185.1z"
+		/>
+		<g transform="rotate(-5.65 205.392 341.466)">
+			<circle
+				cx={205.16}
+				cy={341.17}
+				r={37.85}
+				style={{
+					fill: '#fff',
+				}}
+			/>
+			<circle
+				cx={205.16}
+				cy={341.17}
+				r={37.85}
+				style={{
+					fill: 'none',
+					stroke: '#cd0000',
+					strokeMiterlimit: 100,
+					strokeWidth: '6.22px',
+				}}
+			/>
+		</g>
+		<path
+			style={{
+				fill: '#cd0000',
+			}}
+			d="M189.56 362.35v-9.5l20.46-11.9-20.46-11.52v-9.85l37 21.38-36.72 21.39"
+		/>
+		<path
+			style={{
+				fill: '#ff9ca1',
+			}}
+			d="m189.56 329.43 20.46 11.52-20.46 11.9"
+		/>
+	</svg>
+);
+
+const videopackLoop = (
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400">
+		<path
+			d="M352.41 199.29c-.01 35.29-28.64 63.89-63.93 63.88-29.13-.01-54.56-19.72-61.85-47.92l-.17-.73-.24-.71-15.43-45.44-.1.05c-17.16-54.81-75.5-85.33-130.31-68.17-43.31 13.56-72.82 53.64-72.92 99.02-.01 57.4 46.51 103.95 103.91 103.97 13 0 25.88-2.43 37.98-7.18l-14.62-37.27c-32.86 12.87-69.94-3.33-82.81-36.19s3.33-69.94 36.19-82.81 69.94 3.33 82.81 36.19c.94 2.39 1.73 4.84 2.37 7.33l.24-.07 14.5 42.78c14.88 55.47 71.91 88.38 127.38 73.5 45.38-12.17 76.96-53.25 77.05-100.24.01-57.4-46.51-103.95-103.92-103.96-12.97 0-25.82 2.42-37.9 7.15l14.59 37.29c32.87-12.85 69.94 3.39 82.78 36.26 2.9 7.41 4.38 15.3 4.38 23.26"
+			style={{
+				fill: '#cd0000',
+			}}
+		/>
+		<path
+			style={{
+				fill: '#cd0000',
+			}}
+			d="M90.65 230.42v-13.6l29.3-17.05-29.3-16.49v-14.1l52.99 30.62-52.58 30.62"
+		/>
+		<path
+			style={{
+				fill: '#ff9ca1',
+			}}
+			d="m90.65 183.28 29.3 16.49-29.3 17.05"
+		/>
+		<path
+			style={{
+				fill: '#cd0000',
+			}}
+			d="M266.39 230.42v-13.6l29.29-17.05-29.29-16.49v-14.1l52.99 30.62-52.58 30.62"
+		/>
+		<path
+			style={{
+				fill: '#ff9ca1',
+			}}
+			d="m266.39 183.28 29.29 16.49-29.29 17.05"
+		/>
+	</svg>
+);
+
+const videopackPagination = (
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400">
+		<rect
+			x={4}
+			y={127.95}
+			width={117.18}
+			height={117.18}
+			rx={7}
+			ry={7}
+			style={{
+				fill: '#cd0000',
+			}}
+		/>
+		<path
+			d="M69.1 210.32h-6.2v-38.03c-2.51 1.67-6.39 2.51-11.64 2.51v-4.88c7.59 0 11.94-2.92 13.06-8.77h4.78v49.18Z"
+			style={{
+				fill: '#fff',
+			}}
+		/>
+		<rect
+			x={145.36}
+			y={131.95}
+			width={109.18}
+			height={109.18}
+			rx={3}
+			ry={3}
+			style={{
+				fill: '#fff',
+			}}
+		/>
+		<path
+			d="M250.54 135.95v101.18H149.36V135.95zm1-8H148.36c-3.87 0-7 3.13-7 7v103.18c0 3.87 3.13 7 7 7h103.18c3.87 0 7-3.13 7-7V134.95c0-3.87-3.13-7-7-7"
+			style={{
+				fill: '#cd0000',
+			}}
+		/>
+		<path
+			d="M215.24 210.32h-32.03c0-2.95.91-5.76 2.74-8.44s5.17-5.99 10.04-9.91 8.21-7.06 10.01-9.4 2.7-5 2.7-7.97c0-2.66-.77-4.75-2.31-6.28s-3.71-2.29-6.5-2.29c-2.42 0-4.49.77-6.2 2.31s-2.68 3.8-2.9 6.79h-6.43c.35-4.24 1.92-7.64 4.7-10.17 2.78-2.54 6.44-3.81 10.97-3.81 4.82 0 8.53 1.29 11.15 3.88 2.62 2.58 3.92 5.82 3.92 9.71 0 3.52-.97 6.62-2.9 9.32-1.94 2.69-5.78 6.44-11.53 11.23q-8.625 7.185-8.79 9.3h23.35v5.74Z"
+			style={{
+				fill: '#cd0000',
+			}}
+		/>
+		<rect
+			x={282.73}
+			y={131.95}
+			width={109.18}
+			height={109.18}
+			rx={3}
+			ry={3}
+			style={{
+				fill: '#fff',
+			}}
+		/>
+		<path
+			d="M387.91 135.95v101.18H286.73V135.95zm1-8H285.73c-3.87 0-7 3.13-7 7v103.18c0 3.87 3.13 7 7 7h103.18c3.87 0 7-3.13 7-7V134.95c0-3.87-3.13-7-7-7"
+			style={{
+				fill: '#cd0000',
+			}}
+		/>
+		<path
+			d="m325.77 171.83 4.27-4.39 26.58 22.19-26.58 22.19-4.27-4.35 21.55-17.76z"
+			style={{
+				fill: '#cd0000',
+			}}
+		/>
+	</svg>
+);
+
+const videopackPlayButton = (
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400">
+		<g transform="rotate(-45 205.37 193.523)">
+			<circle
+				cx={205.37}
+				cy={193.52}
+				r={87.51}
+				style={{
+					fill: '#fff',
+				}}
+			/>
+			<circle
+				cx={205.37}
+				cy={193.52}
+				r={87.51}
+				style={{
+					fill: 'none',
+					stroke: '#cd0000',
+					strokeMiterlimit: 100,
+					strokeWidth: '14.38px',
+				}}
+			/>
+		</g>
+		<path
+			style={{
+				fill: '#cd0000',
+			}}
+			d="M169.29 242.49v-21.96l47.31-27.52-47.31-26.64V143.6l85.58 49.45-84.91 49.44"
+		/>
+		<path
+			style={{
+				fill: '#ff9ca1',
+			}}
+			d="m169.29 166.37 47.31 26.64-47.31 27.52"
+		/>
+	</svg>
+);
+
+const videopackPlayer = (
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400">
+		<circle
+			cx={197.8}
+			cy={200.99}
+			r={69.82}
+			style={{
+				fill: '#fff',
+			}}
+		/>
+		<circle
+			cx={197.8}
+			cy={200.99}
+			r={69.82}
+			style={{
+				fill: 'none',
+				stroke: '#cd0000',
+				strokeMiterlimit: 100,
+				strokeWidth: '11.47px',
+			}}
+		/>
+		<path
+			style={{
+				fill: '#cd0000',
+			}}
+			d="M169.02 240.06v-17.52l37.74-21.96-37.74-21.25v-18.16l68.27 39.44-67.74 39.45"
+		/>
+		<path
+			style={{
+				fill: '#ff9ca1',
+			}}
+			d="m169.02 179.33 37.74 21.25-37.74 21.96"
+		/>
+		<path
+			style={{
+				fill: 'none',
+				stroke: '#cd0000',
+				strokeMiterlimit: 100,
+				strokeWidth: 19,
+			}}
+			d="M13.89 96.92h372.22v209.25H13.89z"
+		/>
+	</svg>
+);
+
+const videopackThumbnail = (
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400">
+		<path
+			style={{
+				fill: '#cd0000',
+			}}
+			d="M170.16 207.08v-19.61l42.27-24.6-42.27-23.8v-20.34l76.46 44.18-75.86 44.17"
+		/>
+		<path
+			style={{
+				fill: '#ff9ca1',
+			}}
+			d="m170.16 139.07 42.27 23.8-42.27 24.6M46.7 364.45c-6.58 0-10.96-4.39-10.96-10.96v-52.62l89.9-65.78 65.78 41.66c6.58 4.39 15.35 4.39 19.73-2.19l76.74-74.55 76.74 74.55v78.94c0 6.58-4.39 10.96-10.96 10.96"
+		/>
+		<path
+			d="M353.83 2.67H46.85c-24.12 0-43.86 19.74-43.86 43.86v306.98c0 24.12 19.73 43.85 43.85 43.85h306.98c24.12 0 43.85-19.73 43.85-43.85V46.53c0-24.12-19.73-43.85-43.85-43.85Zm10.96 350.83c0 6.58-4.39 10.96-10.96 10.96H46.85c-6.58 0-10.96-4.39-10.96-10.96v-52.62l89.9-65.78 65.78 41.66c6.58 4.39 15.35 4.39 19.73-2.19l76.74-74.55 76.74 74.55v78.94Zm-85.63-188.64c-.79.64-1.49 1.39-2.08 2.27l-78.94 76.74-63.59-43.85c-.57-.38-1.14-.72-1.71-1.03a77 77 0 0 1-8.68-35.6c0-42.74 34.77-77.51 77.51-77.51s77.51 34.77 77.51 77.51c0 .49-.03.98-.04 1.48Zm85.63 65.85-65.68-63.49c.05-1.28.08-2.56.08-3.84 0-53.77-43.74-97.51-97.51-97.51s-97.51 43.74-97.51 97.51c0 14.16 3.04 27.63 8.49 39.78l-74.58 53.86V46.53c.05-5.27 2.19-10.96 8.77-10.96h306.98c6.58 0 10.96 4.39 10.96 10.96v184.19Z"
+			style={{
+				fill: '#cd0000',
+			}}
+		/>
+	</svg>
+);
+
+const videopackTitle = (
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400">
+		<path
+			style={{
+				fill: '#cd0000',
+			}}
+			d="M1.92 52.81h396.16v285.84H1.92z"
+		/>
+		<path
+			d="M295.68 87.22v44.5h-72.27V311.4h-53.54V131.72H97.6v-44.5z"
+			style={{
+				fill: '#fff',
+			}}
+		/>
+	</svg>
+);
+
+const videopackVideo = (
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400">
+		<circle
+			cx={199.66}
+			cy={198.62}
+			r={47.96}
+			style={{
+				fill: '#fff',
+			}}
+		/>
+		<circle
+			cx={199.66}
+			cy={198.62}
+			r={47.96}
+			style={{
+				fill: 'none',
+				stroke: '#cd0000',
+				strokeMiterlimit: 100,
+				strokeWidth: '7.88px',
+			}}
+		/>
+		<path
+			style={{
+				fill: '#cd0000',
+			}}
+			d="M179.89 225.45v-12.03l25.92-15.09-25.92-14.59v-12.48l46.9 27.1-46.54 27.09"
+		/>
+		<path
+			style={{
+				fill: '#ff9ca1',
+			}}
+			d="m179.89 183.74 25.92 14.59-25.92 15.09"
+		/>
+		<path
+			style={{
+				fill: 'none',
+				stroke: '#cd0000',
+				strokeMiterlimit: 100,
+				strokeWidth: '13.05px',
+			}}
+			d="M73.32 127.13h255.7v143.75H73.32z"
+		/>
+		<path
+			style={{
+				fill: 'none',
+				stroke: '#cd0000',
+				strokeMiterlimit: 100,
+				strokeWidth: '7.16px',
+			}}
+			d="M3.38 3.56h393.28v393.28H3.38z"
+		/>
+	</svg>
+);
+
+const videopackViewCount = (
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400">
+		<path
+			style={{
+				fill: '#ff9ca1',
+			}}
+			d="m8.57 162.7 71.81 40.43-71.81 41.79"
+		/>
+		<path
+			style={{
+				fill: '#cd0000',
+			}}
+			d="M8.46 250.14V227.9l47.92-27.88-47.92-26.98v-23.05l86.68 50.07-86 50.08m169.59-93.16-6.47 24.1h19.53l6.47-24.1h11.41l-6.47 24.1h12.94v9.51h-15.47l-4.95 18.39h14.2v9.51h-16.74l-6.21 23.46h-11.41l6.21-23.46h-19.53l-6.21 23.46h-11.41l6.21-23.46h-13.57v-9.51h16.11l4.95-18.39h-14.84v-9.51h17.37l6.47-24.1zm-13.95 51.99h19.53l4.95-18.39h-19.53zm101.7-51.99-6.47 24.1h19.53l6.47-24.1h11.41l-6.47 24.1h12.94v9.51h-15.47l-4.95 18.39h14.2v9.51h-16.74l-6.21 23.46h-11.41l6.21-23.46h-19.53l-6.21 23.46h-11.41l6.21-23.46h-13.57v-9.51h16.11l4.95-18.39h-14.84v-9.51h17.37l6.47-24.1zm-13.95 51.99h19.53l4.95-18.39h-19.53zm101.7-51.99-6.47 24.1h19.53l6.47-24.1h11.41l-6.47 24.1h12.94v9.51h-15.47l-4.95 18.39h14.2v9.51h-16.74l-6.21 23.46h-11.41l6.21-23.46h-19.53l-6.21 23.46h-11.41l6.21-23.46h-13.57v-9.51h16.11l4.95-18.39h-14.84v-9.51h17.37l6.47-24.1zm-13.95 51.99h19.53l4.95-18.39h-19.53z"
+		/>
+	</svg>
+);
+
+const videopackWatermark = (
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400">
+		<g
+			style={{
+				opacity: 0.2,
+			}}
+		>
+			<g transform="rotate(-45 201.506 200.804)">
+				<circle
+					cx={201.51}
+					cy={200.8}
+					r={121.45}
+					style={{
+						fill: '#fff',
+					}}
+				/>
+				<circle
+					cx={201.51}
+					cy={200.8}
+					r={121.45}
+					style={{
+						fill: 'none',
+						stroke: '#cd0000',
+						strokeMiterlimit: 100,
+						strokeWidth: '19.96px',
+					}}
+				/>
+			</g>
+			<path
+				style={{
+					fill: '#cd0000',
+				}}
+				d="M133.55 150.74h30.48l38.2 65.65 36.97-65.65h31.58l-68.61 118.75-68.62-117.83"
+			/>
+			<path
+				style={{
+					fill: '#ff9ca1',
+				}}
+				d="m239.2 150.74-36.97 65.65-38.2-65.65"
+			/>
+		</g>
 	</svg>
 );
 
@@ -140,13 +874,27 @@ const playOutline = (
 
 export {
 	insertImage,
-	save,
-	videopack,
-	volumeDown,
-	volumeUp,
-	sortAscending,
-	sortDescending,
+	pause,
 	play,
 	playOutline,
-	pause,
+	save,
+	sortAscending,
+	sortDescending,
+	volumeDown,
+	volumeUp,
+	videopack,
+	videopackCaption,
+	videopackCollection,
+	videopackDuration,
+	videopackGallery,
+	videopackList,
+	videopackLoop,
+	videopackPagination,
+	videopackPlayButton,
+	videopackPlayer,
+	videopackThumbnail,
+	videopackTitle,
+	videopackVideo,
+	videopackViewCount,
+	videopackWatermark,
 };
